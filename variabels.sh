081d44a1e67d270e2429dab743ef0b1a7503095a
Umair Khan <81556052+umair2607@users.noreply.github.com>
2022-10-29 18:08:15 +0500

    Add files via upload

---
+++ variabels.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# System Defined Variables
+
+echo $HOME # Path of Home Directory
+echo $PWD # Current Directory
+echo $BASH # Bash
+echo $BASH_VERSION # Bash Version
+echo $LOGNAME # Name of Login User
+echo $OSTYPE # Types of OS
+
+# User Defined Variables
+
+name="Umair"
+Roll_No=01-134202-114
+
+echo "Student name is $name roll number is $Roll_No"

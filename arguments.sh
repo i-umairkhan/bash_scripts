cd216575ce5c9f79e417b2b49845719757a90a81
Umair Khan <81556052+umair2607@users.noreply.github.com>
2022-10-29 18:06:38 +0500

    Add files via upload

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Passing arguments to files and using them as variabels
+
+echo "File name is $0"
+echo $3
+Data=$5
+echo "A $Data costs just $6"
+echo $# # it shows numbar of arguments passed
